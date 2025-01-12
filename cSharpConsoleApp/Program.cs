b45204316e35ab4e993864b9a01cebf2eb88d209
dingdong <dingdong@PVD816D>
2025-01-12 17:31:54 +0100

    test

---
+++ cSharpConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Business.Data;
+using Business.Interfaces;
+using Business.Services;
+using cSharpConsoleApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        var serviceProvider = services.BuildServiceProvider();
+        var userConsoleInterface = serviceProvider.GetRequiredService<UserConsoleInterface>();
+        userConsoleInterface.Run();
+    }
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IDataStorage, JsonStorage>();
+        services.AddScoped<IUserService, UserManager>();
+        services.AddScoped<UserConsoleInterface>();
+    }
+}
+
